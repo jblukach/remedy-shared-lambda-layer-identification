44517ddc99d4af859ab50f7c7037ca81999e5cc0
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-02-10 02:54:07 +0000

    layer iam policies

---
+++ remedy/remedy.py
@@ -0,0 +1,45 @@
+import boto3
+import json
+import logging
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def handler(event, context):
+    
+    client = boto3.client('ec2')
+    regions = client.describe_regions()
+    
+    for region in regions['Regions']:
+        
+        lambda_client = boto3.client('lambda', region_name = region['RegionName'])
+        
+        paginator = lambda_client.get_paginator('list_layers')
+        response_iterator = paginator.paginate()
+        
+        for page in response_iterator:
+            for item in page['Layers']:
+                    
+                    paginator2 = lambda_client.get_paginator('list_layer_versions')
+                    response_iterator2 = paginator2.paginate(LayerName = item['LayerName'])
+                    
+                    for page2 in response_iterator2:
+                        for item2 in page2['LayerVersions']:
+                            
+                            print('Name: '+item['LayerName']+' Version: '+str(item2['Version']))
+                            
+                            try:
+                                iam_policy = lambda_client.get_layer_version_policy(
+                                    LayerName = item['LayerName'],
+                                    VersionNumber = item2['Version']
+                                )
+                                print(iam_policy)
+                            except:
+                                print('No Policy!')
+                                pass
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Identify Shared Lambda Layers')
+    }
--- remedy_shared_lambda_layer_identification/remedy_shared_lambda_layer_identification_stack.py
+++ remedy_shared_lambda_layer_identification/remedy_shared_lambda_layer_identification_stack.py
@@ -15,3 +15,56 @@ class RemedySharedLambdaLayerIdentificationStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        role = _iam.Role(
+            self, 'role', 
+            assumed_by = _iam.ServicePrincipal(
+                'lambda.amazonaws.com'
+            )
+        )
+        
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AWSLambdaBasicExecutionRole'
+            )
+        )
+        
+        role.add_to_policy(
+            _iam.PolicyStatement(
+                actions = [
+                    'ec2:DescribeRegions',
+                    'lambda:ListLayers',
+                    'lambda:ListLayerVersions',
+                    'lambda:GetLayerVersionPolicy'
+                ],
+                resources = ['*']
+            )
+        )
+
+        remedy = _lambda.Function(
+            self, 'remedy',
+            code = _lambda.Code.from_asset('remedy'),
+            handler = 'remedy.handler',
+            runtime = _lambda.Runtime.PYTHON_3_9,
+            architecture = _lambda.Architecture.ARM_64,
+            timeout = Duration.seconds(900),
+            memory_size = 128,
+            role = role
+        )
+       
+        logs = _logs.LogGroup(
+            self, 'logs',
+            log_group_name = '/aws/lambda/'+remedy.function_name,
+            retention = _logs.RetentionDays.ONE_DAY,
+            removal_policy = RemovalPolicy.DESTROY
+        )
+
+        #provider = _custom.Provider(
+        #    self, 'provider',
+        #    on_event_handler = remedy
+        #)
+
+        #resource = CustomResource(
+        #    self, 'resource',
+        #    service_token = provider.service_token
+        #)
